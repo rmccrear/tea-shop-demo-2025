edcd012b0694210fba4052183c171de4dd3b14a8
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-09-26 19:22:00 -0700

    add array to save orders

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const PORT = 3000;
 
 app.use(express.static("public"));
 
+const orders = [
+    'An order for Robert of jasmine tea x 2',
+    'An order for Clay of Green Tea x 3',
+    'An order for Dylan of Black Tea x 1'
+];
+
 app.get("/", (req, res) => {
-    res.send("Hello from /");
+    res.send(`<a href="/order-form.html">order a drink</a>`);
 });
 
 app.get("/complete-order", (req, res) => {
@@ -13,10 +19,35 @@ app.get("/complete-order", (req, res) => {
     const product = req.query.product;
     const quantity = req.query.quantity;
 
+    orders.push(`An order for ${firstName} of ${product} x ${quantity}`);
+
+    console.log(orders);
+
     res.send(`
             <h1> Thank you for your order </h1>
             <p> Thanks ${firstName}! </p>
             <p> Your ${product} x ${quantity} will be ready shortly </p>
+            <p><a href="/order-form.html">Order a new one</a></p>
+        `);
+});
+
+app.get("/current-orders", (req, res)=>{
+    const orderNo = req.query.orderNo;
+    res.send("hello..." + orders[orderNo]);
+});
+
+app.get("/all-orders", (req, res)=>{
+    const htmlListItems = orders.map((order) => {
+        return `<li> ${order} </li>`
+    });
+    console.log(htmlListItems);
+    const html = htmlListItems.join("\n");
+    console.log(html);
+    res.send(`
+        <!DOCTYPE HTML>
+            <ol>
+                ${html}
+            </ol>
         `);
 });
 
